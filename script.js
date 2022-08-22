0f37166ce80cfdc68c7a93f25c80049caa03a60a
Малыбай Анар Ерболаткызы <MalybayA@lcoast.itstep>
2022-08-22 21:42:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let num1 = prompt();
+const result = +num1 /0.621371;
+alert(result)
+//let a = prompt ();
+//let b = prompt ();
+let x = (b * -1)/a
+alert (x)
+
+
+
+let a = 463;
+
+//let last = a % 10;
+
+a = a - last;
+
+a = a / 10;
+
+a= a % 10;
+
+console.log(a);
+
+
+let b = 123456;
+
+
+let last = a % 10;
+a = ((last * 100000 + a) - last) / 10
+
+console.log(a); 
+
+
+
+let 
+
+
+
+
